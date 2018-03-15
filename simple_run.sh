997d91335e921ab112072ffbcdca49062970fc18
vduseev <vagiz.d@gmail.com>
2018-03-15 06:58:37 +0100

    Replaced tab based indentation symbols in file with double spaces

--- simple_run.sh
+++ simple_run.sh
@@ -3,50 +3,50 @@
 
 compile()
 {
-	#debug
-	echo $(find "$1" -name '*.java');
-	
+  #debug
+  echo $(find "$1" -name '*.java');
 
-	#create folder if it doesn't exist
-	if [ ! -d "$PWD/out" ]
-	then
-		mkdir "$PWD/out"
-	fi
 
-	#compile recursively all files to current folder/out
-	javac -d "$PWD/out" $(find "$1" -name '*.java');
+  #create folder if it doesn't exist
+  if [ ! -d "$PWD/out" ]
+  then
+    mkdir "$PWD/out"
+  fi
 
-	#That part doesn't work now. There should be insterting main class 
-	echo "Main-Class: Sample" > "$PWD/out/manifest.mf"
+  #compile recursively all files to current folder/out
+  javac -d "$PWD/out" $(find "$1" -name '*.java');
 
-	
-	#echo $(find "$PWD/out" -name '*.class')
-	
-	local OUT=$(echo $PWD/out | rev | cut -d'/' -f-3 | rev)
-	
-	#echo "$PWD/out" | rev | cut -d'/' -f-3 | rev
-	
-	echo "out = $OUT"
-	
-	#creating jar
-	jar cvfm "$PWD/out/sample.jar" "$PWD/out/manifest.mf" $(find "$PWD/out" -name '*.class')
+  #That part doesn't work now. There should be insterting main class
+  echo "Main-Class: Sample" > "$PWD/out/manifest.mf"
+
+
+  #echo $(find "$PWD/out" -name '*.class')
+
+  local OUT=$(echo $PWD/out | rev | cut -d'/' -f-3 | rev)
+
+  #echo "$PWD/out" | rev | cut -d'/' -f-3 | rev
+
+  echo "out = $OUT"
+
+  #creating jar
+  jar cvfm "$PWD/out/sample.jar" "$PWD/out/manifest.mf" $(find "$PWD/out" -name '*.class')
 }
 
 main()
 {
-	if [ $# -eq 0 ]
-	then
-		echo "You didn't specify src folder, wanna use $PWD/src y/n ?"
-	 	read decision
-		if [ $decision = "y" ] || [ $decision = "Y" ]
-		then
-			local PAC=`grep -r -n -i --include="*.java" "static void main" .`
-			echo ${PAC} | sed -E 's/^\.\/src\/(.+)\/[^\/]+\.java:[0-9]+: .+$/\1/'
-			compile "$PWD/src"
-		fi
-	else
-			compile $1
-	fi
+  if [ $# -eq 0 ]
+  then
+    echo "You didn't specify src folder, wanna use $PWD/src y/n ?"
+     read decision
+    if [ $decision = "y" ] || [ $decision = "Y" ]
+    then
+      local PAC=`grep -r -n -i --include="*.java" "static void main" .`
+      echo ${PAC} | sed -E 's/^\.\/src\/(.+)\/[^\/]+\.java:[0-9]+: .+$/\1/'
+      compile "$PWD/src"
+    fi
+  else
+      compile $1
+  fi
 }
 
 main
